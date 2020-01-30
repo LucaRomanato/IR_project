26183ebd6057a809d38cdf7c0ba281515f3edcd9
LucaRomanato <35804508+LucaRomanato@users.noreply.github.com>
2020-01-30 16:47:02 +0100

    Update Query

--- Script/Query.py
+++ Script/Query.py
@@ -1,6 +1,6 @@
 import requests
 from elasticsearch import Elasticsearch
-from Script.userPreprocess import preProcess
+from userPreprocess import preProcess
 import elasticsearch as es
 
 def connectToELServer ():
@@ -16,14 +16,51 @@ client, es = connectToELServer();
 
 #####################
 bow = preProcess()
+print("BOW\n")
 print(bow)
 ####################
 
 #query prova base
-res = es.search(index='index',body={'query':{'match':{'text':'#SolarOrbiter'}}})
+#res = es.search(index='index',body={'query':{'match':{'text':'#SolarOrbiter'}}})
+#print("%d documents found" % res['hits']['total']['value'])
+#for doc in res['hits']['hits']:
+#    print("id: %s, score: %s ) %s" % (doc['_id'], doc['_score'], doc['_source']['text']))
+#    print("\n")
+
+#Query BoW
+
+text_query = "Trump"
+must = []
+must.append({"query_string":{ "query":text_query, "default_field": "text"}})
+
+should = []
+for i in range(0,20):
+        should.append({"match" : { "text": bow[i]}})
+        
+#must.append({"query_string":{ "query":query, "default_field": "text"}})
+#should.append({"match": {"text": bow}})
+#q = {"must": must,"should": should}
+
+# Take the user's parameters and put them into a
+# Python dictionary structured as an Elasticsearch query:
+query_body = {
+        "query":{
+                "function_score":{
+                 "query": {
+                         "bool" : { 
+                                 "should": should,
+                                 "must": must,
+                                  }  
+                          }
+                         }
+                        }
+ 
+}
+
+# Pass the query dictionary to the 'body' parameter of the
+# client's Search() method, and have it return results:
+res = es.search(index='index',body=query_body)
 print("%d documents found" % res['hits']['total']['value'])
 for doc in res['hits']['hits']:
-    print("id: %s, score: %s ) %s" % (doc['_id'], doc['_score'], doc['_source']['text']))
+    print("id: %s, score: %s, user: %s ) %s" % (doc['_id'], doc['_score'], doc['_source']['user'], doc['_source']['text']))
     print("\n")
-
-#query prova con BoW
